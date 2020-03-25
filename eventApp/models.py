8d664cb1a93f7c26ea4009c06854fa6b56008013
Apseuma <37141126+Apseuma@users.noreply.github.com>
2020-03-25 09:55:22 +0100

    models.py adaptation

--- eventApp/models.py
+++ eventApp/models.py
@@ -3,37 +3,40 @@ from django.db import models
 
 
 # Create your models here.
-class Customer(User):
+class CustomUser(User):
     is_adult = models.BooleanField()
     last_update = models.DateField()
+    modified_by = models.ForeignKey('self', on_delete=models.SET_NULL)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return u"%s" % self.username
 
 
-class Space(models.Model):
-    FIELD_OPTIONS = (('football_field', 'Football field'), ('swimming_pool', 'Swimming pool'))
+class Field(models.Model):
+    kind_of_field = models.CharField(max_length=50, primary_key=True)
 
-    field = models.CharField(max_length=50, choices=FIELD_OPTIONS)
+
+class Space(models.Model):
+    field = models.ForeignKey(Field, on_delete=models.PROTECT)
     available_since = models.TimeField()
     available_until = models.TimeField()
     price_per_hour = models.IntegerField
     size = models.IntegerField()  # review if necessary
     photo = models.ImageField()
     description = models.TextField(blank=True, null=True)
-    offer = models.FloatField(default=0)  # percentage by the moment
+    offer = models.FloatField()  # percentage by the moment
     last_update = models.DateField()
-    modified_by = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    modified_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
-        return u"%s %d" % (self.field, self.id)
+        return u"%s" % self.field, self.id
 
 
 class Reservation(models.Model):
     event_name = models.CharField(max_length=100)
-    organizer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name="organizer")
+    organizer = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
     space = models.ForeignKey(Space, on_delete=models.PROTECT)
     reservation_date = models.DateField(auto_now_add=True)
     event_date = models.DateField()
@@ -42,7 +45,7 @@ class Reservation(models.Model):
     price = models.IntegerField()
     is_paid = models.BooleanField(default=False)
     last_update = models.DateField()
-    modified_by = models.ForeignKey(Customer, on_delete=models.SET_DEFAULT, default=organizer)
+    modified_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
